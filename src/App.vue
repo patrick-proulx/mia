67173e2866b88684b3c7b0c0d2f503dd7cf734ef
Patrick Proulx <patrick@knak.com>
2021-03-27 13:13:05 -0400

    Original commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="MIA - My Improved Allowance"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+</style>
